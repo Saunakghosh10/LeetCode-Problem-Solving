33f977b90e1e1a619878e39cb159bee9ea0db3c0
Saunak <76943154+Saunakghosh10@users.noreply.github.com>
2023-06-22 23:08:06 +0530

    Time: 112 ms (89.57%), Space: 59.8 MB (65.94%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -4,7 +4,7 @@ public:
         int n = prices.size();
         vector<int> free(n, 0), hold(n, 0);
         
-        // In order to hold a stock on day 0, we have no other choice but to buy it for prices[0].
+ 
         hold[0] = -prices[0];
         
         for (int i = 1; i < n; i++) {
